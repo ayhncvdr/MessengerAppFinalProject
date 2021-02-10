b099978f11710d272d77a9d165e35ed77844c8d8
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-02-10 14:27:05 +0300

    TabsPage, TabController and TabBar implemented!

--- lib/Pages/Tabs.dart
+++ lib/Pages/Tabs.dart
@@ -1,16 +1,32 @@
 import 'package:chat_app_final/Pages/SignIn.dart';
+import 'package:chat_app_final/TabPages/CallsPage.dart';
+import 'package:chat_app_final/TabPages/CameraPage.dart';
+import 'package:chat_app_final/TabPages/ChatsPage.dart';
+import 'package:chat_app_final/TabPages/StatusPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Tabs extends StatefulWidget {
   @override
   _TabsState createState() => _TabsState();
 }
 
-class _TabsState extends State<Tabs> {
+class _TabsState extends State<Tabs> with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(initialIndex: 1, length: 4, vsync: this);
+
+    super.initState();
+  }
+
   FirebaseAuth _auth = FirebaseAuth.instance;
   signOut() async {
     try {
+      /* SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.clear();*/
       return await _auth.signOut().then((value) {
         Navigator.push(
             context, MaterialPageRoute(builder: (context) => SignIn()));
@@ -24,7 +40,11 @@ class _TabsState extends State<Tabs> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chats"),
+        backgroundColor: Colors.indigo,
+        title: Text(
+          "MessengerApp",
+          style: GoogleFonts.getFont("Lobster"),
+        ),
         actions: [
           GestureDetector(
             onTap: signOut,
@@ -33,6 +53,27 @@ class _TabsState extends State<Tabs> {
             ),
           )
         ],
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "Chats",
+            ),
+            Tab(
+              text: "Status",
+            ),
+            Tab(
+              text: "Calls",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [CameraPage(), ChatsPage(), StatusPage(), CallsPage()],
       ),
     );
   }
