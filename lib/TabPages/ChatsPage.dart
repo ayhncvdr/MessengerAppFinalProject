5c352e40f4dc33d45822d9841cd100ff103e04c5
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-02-12 18:39:05 +0300

    Searching Users Completed!

--- lib/TabPages/ChatsPage.dart
+++ lib/TabPages/ChatsPage.dart
@@ -1,6 +1,8 @@
+import 'package:chat_app_final/Pages/Conversations.dart';
 import 'package:chat_app_final/Services/DatabaseMethods.dart';
 import 'package:chat_app_final/widgets/widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatsPage extends StatefulWidget {
@@ -17,6 +19,43 @@ class _ChatsPageState extends State<ChatsPage> {
     await DatabaseMethods().getUserNamefromDB(_textEditingController.text);
   }
 
+  Widget searchUsersListTile(String imgUrl, name, email) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Conversations()));
+      },
+      child: Row(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.05,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              imgUrl,
+              height: MediaQuery.of(context).size.height * 0.05,
+              width: MediaQuery.of(context).size.height * 0.05,
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.02,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name),
+              Text(email),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          )
+        ],
+      ),
+    );
+  }
+
   Widget searchUsersList() {
     return FutureBuilder(
         future:
@@ -28,7 +67,14 @@ class _ChatsPageState extends State<ChatsPage> {
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
                     DocumentSnapshot ds = snapshot.data.docs[index];
-                    return Image.network(ds["imgUrl"]);
+                    return searchUsersListTile(
+                        ds["imgUrl"], ds["name"], ds["email"]);
+
+                    /*Image.network(
+                      ds["imgUrl"],
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      width: MediaQuery.of(context).size.height * 0.04,
+                    );*/
                   },
                 )
               : Container(
@@ -39,32 +85,29 @@ class _ChatsPageState extends State<ChatsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
+    return Container(
+      child: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 500),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  Expanded(
-                      child: TextField(
-                    controller: _textEditingController,
-                    decoration: textFieldInputDecoration("search users"),
-                  )),
-                  GestureDetector(
-                    child: Icon(Icons.search),
-                    onTap: () {
-                      if (_textEditingController.text != "") {
-                        onSearchButtonClicked();
-                      }
-                    },
-                  ),
-                ],
-              ),
+            Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.05,
+                ),
+                Expanded(
+                    child: TextField(
+                  controller: _textEditingController,
+                  decoration: textFieldInputDecoration("search users"),
+                )),
+                GestureDetector(
+                  child: Icon(Icons.search),
+                  onTap: () {
+                    if (_textEditingController.text != "") {
+                      onSearchButtonClicked();
+                    }
+                  },
+                ),
+              ],
             ),
             searchUsersList(),
           ],
